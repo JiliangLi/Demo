5a52a72b14266209aad7143b7109c5e461e7abbb
Eric <eli20@choate.edu>
2018-10-26 17:35:41 -0400

    class2
    class2

--- class2.py
+++ class2.py
@@ -14,22 +14,25 @@
 # transfer
 import random
 import string
-
-account_number_numbers = [i for i in range(10)]
-random.shuffle(account_number_numbers)
-account_number_letters= random.sample([j for j in list(string.ascii_lowercase)], 10)
 account_number = ""
 
-for i in range(10):
-	rand = random.randrange(2)
-	if rand == 0:
-		account_number += str(account_number_letters[i][0])
-	else:
-		account_number += str(account_number_numbers[i])
+def accountnumber():
+	global account_number
+	account_number_numbers = [i for i in range(10)]
+	random.shuffle(account_number_numbers)
+	account_number_letters= random.sample([j for j in list(string.ascii_lowercase)], 10)
+
+	for i in range(10):
+		rand = random.randrange(2)
+		if rand == 0:
+			account_number += str(account_number_letters[i][0])
+		else:
+			account_number += str(account_number_numbers[i])
 
 class Account:			# class starts with a capital letter and the term should be singular
 	# constructor
 	def __init__(self):
+		accountnumber()
 		self.balance = 0
 		self.account = account_number
 		self.pin = str(277)
